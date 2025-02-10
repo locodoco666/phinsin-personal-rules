08dc58ed730af6c1f6524c690788aaa28d33d564
Phinsin-personal <168050204+Phinsin-personal@users.noreply.github.com>
2025-02-10 20:14:02 +0800

    更新 rename.js

--- rename.js
+++ rename.js
@@ -43,7 +43,7 @@
 const inArg = $arguments; // console.log(inArg)
 const nx = inArg.nx || false,
   bl = inArg.bl || true,
-  nf = inArg.nf || false,
+  nf = inArg.nf || true,
   key = inArg.key || false,
   blgd = inArg.blgd || false,
   blpx = inArg.blpx || false,
